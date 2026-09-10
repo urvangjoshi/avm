6dffcbae70944bf96e7728dfbf3b170d487488b1
Urvang Joshi <urvang@google.com>
2026-09-10 15:02:03 -0700

    Unrestricted partitioning depth for H partitions at speed 1
    Keep ext_recur_depth_level = 0, to allow unrestricted partitioning for H
    partitions.
    Only affects speed 1 because H partitions are disabled at speed >= 2.
    
    Speed 1 CTC 33 frames RA:
    
    ```
    +---------+--------+-------------+
    | Summary |  YUV   |   Enc-time  |
    +---------+--------+-------------+
    |   A1    | -0.15% |   103.8%   |
    |   A2    | -0.22% |   107.0%   |
    |Avg wo B2| -0.27% |   108.0%   |
    +---------+--------+-------------+
    ```
    
    STATS_CHANGED for speed 1

--- av2/encoder/speed_features.c
+++ av2/encoder/speed_features.c
@@ -1255,8 +1255,6 @@ static AVM_INLINE void set_erp_speed_features(AV2_COMP *cpi) {
   }
 
   if (cpi->speed >= 1) {
-    // Emulate erp_pruning_level = 6.
-    sf->part_sf.ext_recur_depth_level = 1;
     sf->part_sf.ml_early_term_after_part_split_level = 2;
     sf->part_sf.prune_part_with_neighbor_boundaries = 1;
   }
